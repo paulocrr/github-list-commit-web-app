a8d0a48a7e37af92343a8a0ad0e0f34b31e32c62
Paulo Rodriguez <paulo_crr@hotmail.com>
2021-11-21 02:00:57 -0500

    feat(api)!: filtered api necessary data

--- server/index.js
+++ server/index.js
@@ -1,29 +1,66 @@
 require('dotenv').config();
-const { Octokit, App, Action } = require("octokit");
+/** Server Configuration */
 const express = require('express');
 const app = express();
-const port = process.env.PORT;
+const port = 3000;
 const apiVersion = 'v1';
-const githubAccessKey = process.env.GITHUB_TOKEN;
 
+/** Github API Configuration */
+const { Octokit} = require("octokit");
+const repoOwner = 'paulocrr';
+const repoName = 'github-list-commit-web-app';
+const repoData = {
+    owner: repoOwner,
+    repo: repoName
+}
+const octokit = new Octokit();
 
-app.get('/', (req, res) => {
+app.get('/', (_, res) => {
   res.send('Hello World!');
 });
 
-app.get(`/api/${apiVersion}/commits`, async (req, res) => {
+app.get(`/api/${apiVersion}/commits`, async (_, res) => {
+
+    let result = {'status': 0,message: '', data:[]};
+    try{
+        const requestResult = await octokit.request('GET /repos/{owner}/{repo}/commits', repoData);
+        const status = requestResult['status'];
+        result['status'] = status;
+
+        for(const commit of requestResult['data']){
+            const commitData = commit['commit'];
+            const filesCommitted = await octokit.request(`GET /repos/{owner}/{repo}/git/trees/${commitData['tree']['sha']}`,repoData);
+            const filteredCommitData = {
+                sha: commit['sha'],
+                date: commitData['committer']['date'],
+                committer: {
+                    name: commitData['committer']['name'],
+                    email: commitData['committer']['email'],
+                    username: commit['committer']['login'],
+                    profile_url: commit['committer']['url'] 
+                },
+                message: commit['commit']['message'],
+                files: filesCommitted['data']['tree'].map(file => file['path'])
+            }
+            result.data.push(filteredCommitData);
+        }
+
+        result['message'] = 'OK';
+
+    }catch(e){
+
+        result['status'] = e['status'];
+        result['message'] = e['response']['data']['message'];
+
+    }finally{
+
+        res.json(result);
+
+    }
     
-    const repoOwner = 'paulocrr';
-    const repoName = 'github-list-commit-web-app';
-    const octokit = new Octokit({auth: githubAccessKey});
-    let result = await octokit.request('GET /repos/{owner}/{repo}/commits', {
-        owner: repoOwner,
-        repo: repoName
-    });
-    console.log(result['data']);
-    res.json(result);
+
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Server listening at http://localhost:${port}`);
 });
